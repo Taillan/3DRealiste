8ec41141557a712fe4ccf8fb14a5a3f7102aafff
richard-ioi <59491347+richard-ioi@users.noreply.github.com>
2021-10-24 15:09:22 +0200

    Modification architecture Objet3D + Ajout parralelepipede3D

---
+++ projet_esiee_2021/Projet_IMA/Objet3D.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Projet_IMA
+{
+    abstract class Objet3D
+    {
+        protected V3 CentreObjet { get; set; }
+        protected Couleur CouleurObjet { get; set; }
+        protected Couleur CouleurAmbiante{ get; set; }
+        protected Lumiere Lumiere { get; set; }
+        protected float CoefficientDiffus { get; set; }
+        
+        public Objet3D(V3 centre, Couleur couleur, Lumiere lumiere, float coefficient_diffus)
+        {
+            CentreObjet = centre;
+            CouleurObjet = couleur;
+            Lumiere = lumiere;
+            CoefficientDiffus = coefficient_diffus;
+            CouleurAmbiante = new Couleur(this.CouleurObjet * Lumiere.Couleur);
+        }
+
+        public abstract void Draw(float pas);
+
+        public Couleur getCouleurDiffuse(float x3D, float y3D, float z3D)
+        {
+            V3 normalizedPixelNormal = (new V3(x3D - this.CentreObjet.x, y3D - this.CentreObjet.y, z3D - this.CentreObjet.z));
+            normalizedPixelNormal.Normalize();
+
+            float NL = normalizedPixelNormal * Lumiere.NormalizedDirection;
+            Couleur nvCouleurDiffuse = CouleurAmbiante * 0.0008f;
+            if (NL > 0)
+            {
+                nvCouleurDiffuse += CouleurAmbiante * (normalizedPixelNormal * Lumiere.NormalizedDirection) * CoefficientDiffus;//0.006f
+            }
+            return nvCouleurDiffuse;
+        }
+    }
+}
--- projet_esiee_2021/Projet_IMA/ProjetEleve.cs
+++ projet_esiee_2021/Projet_IMA/ProjetEleve.cs
@@ -11,17 +11,12 @@ namespace Projet_IMA
         public static void Go()
         {
             Lumiere lumiere = new Lumiere(new V3(1, -1.5f, 1), new Couleur(155, 155, 155));
+            Sphere3D SphereA = new Sphere3D(new V3(200, 200, 300), 150, Couleur.Red, lumiere);
+            SphereA.Draw();
 
-            //////////////////////////////////////////////////////////////////////////
-            ///
-            ///     Sphère en 3D
-            /// 
-            //////////////////////////////////////////////////////////////////////////
-            Sphere3D SphereA = new Sphere3D(new V3(200, 200, 300), 150, Couleur.Red);
-            SphereA.DrawSphere(0.005f, lumiere);
-
-            Sphere3D SphereB = new Sphere3D(new V3(600, 500, 300), 150, Couleur.Green);
-            SphereB.DrawSphere(0.005f, lumiere);
+            Lumiere lumiereb = new Lumiere(new V3(1, 0, 1), new Couleur(155, 155, 155));
+            Parallelepipede3D ParallelepipedeA = new Parallelepipede3D(new V3(500, 500, 200), 150, 150,150, Couleur.Green,lumiereb);
+            ParallelepipedeA.Draw(1);
         }
     }
 }
