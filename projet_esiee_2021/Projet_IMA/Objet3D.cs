eecdefca68f44270997bc6b5c28306983f331501
richard-ioi <59491347+richard-ioi@users.noreply.github.com>
2021-11-17 15:31:51 +0100

    Commentaires Objet3D

--- projet_esiee_2021/Projet_IMA/Objet3D.cs
+++ projet_esiee_2021/Projet_IMA/Objet3D.cs
@@ -7,6 +7,7 @@ namespace Projet_IMA
 {
     abstract class Objet3D
     {
+        #region Attributs
         protected V3 m_CentreObjet { get; set; } 
         private Lumiere m_KeyLumiere { get; set; }
         private Lumiere m_FillLumiere { get; set; }
@@ -17,7 +18,22 @@ namespace Projet_IMA
         private float m_PuissanceSpeculaire { get; set; }
         private float m_CoefficientBumpMap { get; set; }
         protected float m_Pas { get; set; }
+        #endregion
 
+        #region Constructeurs
+        /// <summary>
+        /// Constructeur d'un Objet3D
+        /// </summary>
+        /// <param name="centre">Centre de l'Objet3D</param>
+        /// <param name="key_lumiere">Lumière principale appliquée sur l'Objet3D</param>
+        /// <param name="fill_lumiere">Lumière secondaire appliquée sur l'Objet3D</param>
+        /// <param name="texture">Texture appliquée sur l'Objet3D</param>
+        /// <param name="bump_texture">Texture de bump appliquée sur l'Objet3D</param>
+        /// <param name="coefficient_diffus">Coefficient de diffus de la sphère, plus le coefficient est faible, plus le diffus sera "fondu"</param>
+        /// <param name="coefficient_speculaire">Coefficient spéculaire, plus le coefficient est faible, plus le spéculaire sera "fondu"</param>
+        /// <param name="puissance_speculaire">Puissance spéculaire, plus la puissance est élevée, moins le spéculaire sera grand</param>
+        /// <param name="coefficient_bumpmap">Coefficient de Bump Mapping, plus il sera élevé, plus l'effet 3D sera élevé.</param>
+        /// <param name="pas">Pas de l'Objet3D, plus le pas est grand, plus l'Objet aura de pixels et donc prendra du temps à être tracé</param>
         public Objet3D(V3 centre, Lumiere key_lumiere, Lumiere fill_lumiere, Texture texture, Texture bump_texture, float coefficient_diffus, float coefficient_speculaire, float puissance_speculaire, float coefficient_bumpmap, float pas)
         {
             m_CentreObjet = centre;
@@ -31,22 +47,73 @@ namespace Projet_IMA
             m_CoefficientBumpMap = coefficient_bumpmap;
             m_Pas = pas;
         }
+        #endregion
 
+        #region Méthodes abstraites
+        /// <summary>
+        /// Méthode abstraite déterminant les coordonnées du pixel à tracer en fonction des coordonnées paramétriques (u,v)
+        /// </summary>
+        /// <param name="u">Coordonnée paramétrique u</param>
+        /// <param name="v">Coordonnée paramétrique v</param>
+        /// <returns>Position du pixel à tracer</returns>
         protected abstract V3 getCoords(float u, float v);
+        /// <summary>
+        /// Méthode abstraite déterminant les dérivées partielles des coordonnées du pixel à tracer
+        /// en fonction des coordonnées paramétriques (u,v)
+        /// </summary>
+        /// <param name="u">Coordonnée paramétrique u</param>
+        /// <param name="v">Coordonnée paramétrique v</param>
+        /// <param name="dMdu">Dérivée partielle des coordonnées du pixel en fonction de u</param>
+        /// <param name="dMdv">Dérivée partielle des coordonnées du pixel en fonction de v</param>
         protected abstract void getDerivedCoords(float u, float v, out V3 dMdu, out V3 dMdv);
+        /// <summary>
+        /// Méthode abstraite déterminant la normale du pixel passé en paramètre
+        /// </summary>
+        /// <param name="PixelPosition">Position du pixel dont on veut trouver la normale</param>
+        /// <returns>La normale du pixel passé en paramètre</returns>
         protected abstract V3 getNormal(V3 PixelPosition);
+        /// <summary>
+        /// Méthode abstraite permettant de tracer l'Objet3D
+        /// </summary>
         public abstract void Draw();
+        #endregion
 
+        #region Méthodes
+        /// <summary>
+        /// Méthode permettant d'obtenir la couleur ambiante du pixel
+        /// sur les coordonnées X & Y du repère écran
+        /// </summary>
+        /// <param name="lumiere">Lumière appliquée sur l'Objet3D</param>
+        /// <param name="x_ecran">Coordonnées X du pixel sur le repère écran</param>
+        /// <param name="y_ecran">Coordonnées Y du pixel sur le repère écran</param>
+        /// <returns>Couleur ambiante</returns>
         private Couleur getCouleurAmbiante(Lumiere lumiere, float x_ecran, float y_ecran)
         {
             return m_Texture.LireCouleur(x_ecran, y_ecran) * lumiere.m_Couleur;
         }
 
+        /// <summary>
+        /// Méthode permettant d'obtenir la couleur ambiante abaissée du pixel
+        /// sur les coordonnées X & Y du repère écran
+        /// </summary>
+        /// <param name="lumiere">Lumière appliquée sur l'Objet3D</param>
+        /// <param name="x_ecran">Coordonnées X du pixel sur le repère écran</param>
+        /// <param name="y_ecran">Coordonnées Y du pixel sur le repère écran</param>
+        /// <returns>Couleur ambiante du pixel passée en paramètre</returns>
         private Couleur getLowCouleurAmbiante(Lumiere lumiere, float x_ecran, float y_ecran)
         {
             return getCouleurAmbiante(lumiere, x_ecran,y_ecran) * .0008f;
         }
 
+        /// <summary>
+        /// Méthode permettant d'obtenir la couleur diffuse du pixel
+        /// sur les coordonnées X & Y du repère écran
+        /// </summary>
+        /// <param name="lumiere">Lumière appliquée sur l'Objet3D</param>
+        /// <param name="normalizedPixelNormal">Normale du pixel dont on cherche la couleur diffuse</param>
+        /// <param name="x_ecran">Coordonnées X du pixel sur le repère écran</param>
+        /// <param name="y_ecran">Coordonnées Y du pixel sur le repère écran</param>
+        /// <returns>Couleur diffuse du pixel passée en paramètre</returns>
         private Couleur getCouleurDiffuse(Lumiere lumiere, V3 normalizedPixelNormal, float x_ecran, float y_ecran)
         {
             float cosAlpha = normalizedPixelNormal * lumiere.m_NormalizedDirection;
@@ -60,6 +127,15 @@ namespace Projet_IMA
             }
         }
 
+        /// <summary>
+        /// Méthode permettant d'obtenir la couleur spéculaire du pixel
+        /// sur les coordonnées X & Y du repère écran
+        /// </summary>
+        /// <param name="lumiere">Lumière appliquée sur l'Objet3D</param>
+        /// <param name="PixelPosition">Pixel dont on cherche la couleur spéculaire</param>
+        /// <param name="x_ecran">Coordonnées X du pixel sur le repère écran</param>
+        /// <param name="y_ecran">Coordonnées Y du pixel sur le repère écran</param>
+        /// <returns>Couleur spéculaire du pixel passée en paramètre</returns>
         private Couleur getCouleurSpeculaire(Lumiere lumiere, V3 PixelPosition, V3 N, float x_ecran, float y_ecran)
         {
             V3 L = lumiere.m_Direction;
@@ -78,6 +154,14 @@ namespace Projet_IMA
             }
         }
 
+        /// <summary>
+        /// Méthode permettant d'obtenir la couleur totale du pixel passé en paramètre
+        /// selon les coordonnées paramétriques (u,v)
+        /// </summary>
+        /// <param name="PixelPosition"></param>
+        /// <param name="u">Coordonnée paramétrique u</param>
+        /// <param name="v">Coordonnée paramétrique v</param>
+        /// <returns>Couleur totale du pixel passé en paramètre</returns>
         protected Couleur getCouleur(V3 PixelPosition, float u, float v)
         {
             Lumiere[] lumieres = { m_FillLumiere, m_KeyLumiere };
@@ -99,7 +183,13 @@ namespace Projet_IMA
             }
             return finalColor;
         }
-
+        /// <summary>
+        /// Permet d'obtenir la normale "Bumpée" du pixel passé en paramètre
+        /// </summary>
+        /// <param name="PixelPosition">Coordonnées du pixel dont on veut obtenir la normale bumpée</param>
+        /// <param name="u">Coordonnée paramétrique u</param>
+        /// <param name="v">Coordonnée paramétrique v</param>
+        /// <returns>Normale bumpée du pixel passé en paramètre</returns>
         private V3 getBumpedNormal(V3 PixelPosition, float u, float v)
         {
             V3 N = getNormal(PixelPosition);
@@ -111,5 +201,6 @@ namespace Projet_IMA
 
             return N + K * ((dMdu ^ (N * dhdv)) + ((N * dhdu) ^ dMdv));
         }
+        #endregion
     }
 }
